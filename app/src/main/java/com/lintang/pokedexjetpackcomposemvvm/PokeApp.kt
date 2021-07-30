9e2546985a9d7a3d519ff0b7d367b9f472a99cb4
unknown <lintang@cpssoft.com>
2021-07-30 18:13:57 +0700

    Initial commit

---
+++ app/src/main/java/com/lintang/pokedexjetpackcomposemvvm/PokeApp.kt
@@ -0,0 +1,13 @@
+package com.lintang.pokedexjetpackcomposemvvm
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class PokeApp:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
---
+++ app/src/main/java/com/lintang/pokedexjetpackcomposemvvm/di/PokeModule.kt
@@ -0,0 +1,11 @@
+package com.lintang.pokedexjetpackcomposemvvm.di
+
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object PokeModule {
+}
