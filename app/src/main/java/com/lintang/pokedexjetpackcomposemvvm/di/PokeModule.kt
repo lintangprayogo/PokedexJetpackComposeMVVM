6aee0de24e04aa490ac7e752b2dd2cb1f667efed
Lintang Prayogo <lintangprayogo12@gmail.com>
2021-07-31 15:49:51 +0700

    fix

--- app/src/main/java/com/lintang/pokedexjetpackcomposemvvm/di/PokeModule.kt
+++ app/src/main/java/com/lintang/pokedexjetpackcomposemvvm/di/PokeModule.kt
@@ -1,7 +1,9 @@
 package com.lintang.pokedexjetpackcomposemvvm.di
 
 
+
 import com.lintang.pokedexjetpackcomposemvvm.data.network.PokeApi
+import com.lintang.pokedexjetpackcomposemvvm.repo.PokeRepo
 import com.lintang.pokedexjetpackcomposemvvm.util.Constant.BASE_URL
 import dagger.Module
 import dagger.Provides
@@ -17,9 +19,13 @@ object PokeModule {
 
     @Provides
     @Singleton
-    fun provideApi(): PokeApi =
-        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
-            .build().create(PokeApi::class.java)
+    @JvmStatic
+    fun provideApi():PokeApi=Retrofit.Builder().baseUrl(BASE_URL).
+    addConverterFactory(GsonConverterFactory.create()).build().create(PokeApi::class.java)
 
+    @Provides
+    @Singleton
+    @JvmStatic
+    fun provideRepo(pokeApi: PokeApi):PokeRepo=PokeRepo(pokeApi)
 
 }
